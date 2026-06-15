0a3811b81f03d3700df8e0dc76c06edc85b799c7
aarthvr-b <assefarthur@gmail.com>
2026-06-15 12:53:42 +0200

    adding a database connection with local session

--- app/db.py
+++ app/db.py
@@ -2,3 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 DATABASE_URL = "sqlite:///outbox_demo.db"
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+engine = create_engine(DATABASE_URL, echo=False)
+
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False,
+)
